2148289e46213f2bb7e5164caa71651150c93c6a
Aaron Santiago <aaronsantiago@Aarons-MacBook-Pro.local>
2018-11-12 03:46:16 -0500

    Clean up main.js

--- webpage/main.js
+++ webpage/main.js
@@ -6,108 +6,106 @@ document.body.addEventListener('touchmove', ignoreHandle, { passive: false });
 
 (function(storyContent) {
 
-    var story = new inkjs.Story(storyContent);
+  let story = new inkjs.Story(storyContent);
 
-    var storyContainer = document.querySelectorAll('#story')[0];
+  let storyContainer = document.querySelectorAll('#story')[0];
 
-    function showAfter(delay, el) {
-        setTimeout(function() { el.classList.add("show") }, delay);
+  function showAfter(delay, el) {
+    setTimeout(function() { el.classList.add("show") }, delay);
+  }
+
+  function scrollToBottom() {
+    let progress = 0.0;
+    let start = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    let dist = document.body.scrollHeight - window.innerHeight - start;
+    if( dist < 0 ) return;
+
+    let duration = 300 + 300*dist/100;
+    let startTime = null;
+    function step(time) {
+      if( startTime == null ) startTime = time;
+      let t = (time-startTime) / duration;
+      let lerp = 3*t*t - 2*t*t*t;
+      window.scrollTo(0, start + lerp*dist);
+      if( t < 1 ) requestAnimationFrame(step);
+    }
+    requestAnimationFrame(step);
+  }
+
+  function continueStory() {
+
+    let paragraphIndex = 0;
+    let delay = 0.0;
+
+    // Generate story text - loop through available content
+    while(story.canContinue) {
+
+      // Get ink to generate the next paragraph
+      let paragraphText = story.Continue();
+
+      // Create paragraph element
+      let paragraphElement = document.createElement('p');
+      paragraphElement.innerHTML = paragraphText;
+      storyContainer.appendChild(paragraphElement);
+
+      // Fade in paragraph after a short delay
+      showAfter(delay, paragraphElement);
+
+      delay += 200.0;
     }
 
-    function scrollToBottom() {
-        var progress = 0.0;
-        var start = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-        var dist = document.body.scrollHeight - window.innerHeight - start;
-        if( dist < 0 ) return;
+    story.currentTags.forEach(function(tag) {
+      if(tag.slice(0, 3) == "jds"){
+        let url = "https://jabdownsmash.com/" + tag.slice(4,tag.length);
+        let iframe = document.createElement('iframe');
+        iframe.src = url + "?bust=" + Math.random();
+        document.body.appendChild(iframe);
+        window.addEventListener('message', function (e) {
+            if(e.data.data == "close") {
+              document.body.removeChild(iframe);
+            }
+          }, false);
+      }
+    });
 
-        var duration = 300 + 300*dist/100;
-        var startTime = null;
-        function step(time) {
-            if( startTime == null ) startTime = time;
-            var t = (time-startTime) / duration;
-            var lerp = 3*t*t - 2*t*t*t;
-            window.scrollTo(0, start + lerp*dist);
-            if( t < 1 ) requestAnimationFrame(step);
+    // Create HTML choices from ink choices
+    story.currentChoices.forEach(function(choice) {
+
+      // Create paragraph with anchor element
+      let choiceParagraphElement = document.createElement('p');
+      choiceParagraphElement.classList.add("choice");
+      choiceParagraphElement.innerHTML = `<a href='#'>${choice.text}</a>`
+      storyContainer.appendChild(choiceParagraphElement);
+
+      // Fade choice in after a short delay
+      showAfter(delay, choiceParagraphElement);
+      delay += 200.0;
+
+      // Click on choice
+      let choiceAnchorEl = choiceParagraphElement.querySelectorAll("a")[0];
+      choiceAnchorEl.addEventListener("click", function(event) {
+
+        // Don't follow <a> link
+        event.preventDefault();
+
+        // Remove all existing choices
+        let existingChoices = storyContainer.querySelectorAll('p.choice');
+        for(let i=0; i<existingChoices.length; i++) {
+          let c = existingChoices[i];
+          c.parentNode.removeChild(c);
         }
-        requestAnimationFrame(step);
-    }
 
-    function continueStory() {
+        // Tell the story where to go next
+        story.ChooseChoiceIndex(choice.index);
 
-        var paragraphIndex = 0;
-        var delay = 0.0;
+        // Aaand loop
+        continueStory();
+      });
+    });
 
-        // Generate story text - loop through available content
-        while(story.canContinue) {
+    scrollToBottom();
+  }
 
-            // Get ink to generate the next paragraph
-            var paragraphText = story.Continue();
-
-            // Create paragraph element
-            var paragraphElement = document.createElement('p');
-            paragraphElement.innerHTML = paragraphText;
-            storyContainer.appendChild(paragraphElement);
-
-            // Fade in paragraph after a short delay
-            showAfter(delay, paragraphElement);
-
-            delay += 200.0;
-        }
-
-	story.currentTags.forEach(function(tag) {
-		if(tag.slice(0, 3) == "jds"){
-			let url = "https://jabdownsmash.com/" + tag.slice(4,tag.length);
-			var iframe;
-
-			iframe = document.createElement('iframe');
-			iframe.src = url + "?bust=" + Math.random();
-			document.body.appendChild(iframe);
-			window.addEventListener('message', function (e) {
-					if(e.data.data == "close") {
-						document.body.removeChild(iframe);
-					}
-				}, false);
-		}
-	});
-
-        // Create HTML choices from ink choices
-        story.currentChoices.forEach(function(choice) {
-
-            // Create paragraph with anchor element
-            var choiceParagraphElement = document.createElement('p');
-            choiceParagraphElement.classList.add("choice");
-            choiceParagraphElement.innerHTML = `<a href='#'>${choice.text}</a>`
-            storyContainer.appendChild(choiceParagraphElement);
-
-            // Fade choice in after a short delay
-            showAfter(delay, choiceParagraphElement);
-            delay += 200.0;
-
-            // Click on choice
-            var choiceAnchorEl = choiceParagraphElement.querySelectorAll("a")[0];
-            choiceAnchorEl.addEventListener("click", function(event) {
-
-                // Don't follow <a> link
-                event.preventDefault();
-
-                // Remove all existing choices
-                var existingChoices = storyContainer.querySelectorAll('p.choice');
-                for(var i=0; i<existingChoices.length; i++) {
-                    var c = existingChoices[i];
-                    c.parentNode.removeChild(c);
-                }
-
-                // Tell the story where to go next
-                story.ChooseChoiceIndex(choice.index);
-
-                // Aaand loop
-                continueStory();
-            });
-        });
-
-        scrollToBottom();
-    }
-
-    continueStory();
+  continueStory();
 
 })(storyContent);
